d05eed0dfcae0f92d87d095424200e9d379daeab
unknown <julia_jensen@saojose.sc.senai.br>
2019-06-17 12:16:28 -0300

    queryselectors

--- mod 3/aula 3/2/todos.js
+++ mod 3/aula 3/2/todos.js
@@ -1,1 +1,10 @@
-alert("func")
+var listelement = 
+document.querySelector("#app ul");
+var inputelement =
+document.querySelector("#app input");
+
+var buttonelement =
+document.querySelector("#app button");
+console.log(listelement);
+console.log(inputelement);
+console.log(buttonelement);
