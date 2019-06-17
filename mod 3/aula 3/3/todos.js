bbeb6647d2050f2d08dcaa151f5a3f6fa87735b0
unknown <julia_jensen@saojose.sc.senai.br>
2019-06-17 12:26:09 -0300

    aula 3 mod 3

---
+++ mod 3/aula 3/3/todos.js
@@ -0,0 +1,13 @@
+var listelement = 
+document.querySelector("#app ul");
+var inputelement =
+document.querySelector("#app input");
+
+var buttonelement =
+document.querySelector("#app button");
+
+var todos = [
+"fazer café",
+"beber agua",
+"lixo no lixo"
+];
