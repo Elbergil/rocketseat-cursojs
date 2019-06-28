b353ef67255290bbe40d82048b7cdd8c93174988
unknown <julia_jensen@saojose.sc.senai.br>
2019-06-28 08:57:09 -0300

    links

--- mod 3/aula 3/4/todos.js
+++ mod 3/aula 3/4/todos.js
@@ -15,9 +15,15 @@ listElement.innerHTML = '';
     for (todo of todos){
         var todoElement = document.createElement("li");
         var  todoText = document.createTextNode(todo);
-        
-      todoElement.appendChild(todoText);
+        var linkElement = document.createElement('a')
+        var linkText = document.createTextNode('excluir :3')
+    
+        todoElement.appendChild(todoText);
       listElement.appendChild(todoElement);
+   
+     
+   
+   
     }
 }
 renderTodos();
